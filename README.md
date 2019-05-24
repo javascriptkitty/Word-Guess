db5a7124879b7b10d61f0e1f10d89155d2d849b4
Cristina Alekseeva <cristina.alekseeva@gmail.com>
2019-05-24 00:01:11 -0700

    added audio and README

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # Word-Guess
+
+![Site screenshort](https://github.com/javascriptkitty/Word-Guess/blob/master/assets/images/ScreenShot.png)
+
+A letter-guessing game.
+
+## Description
+
+A theme of this game is 'rock bands'. Computer chooses chooses randomly one of the bands and users get 12 tries to guess it.
+
+## Technoligies Used
+
+- [HTML](https://developer.mozilla.org/en-US/docs/Web/HTML)
+- [CSS](https://developer.mozilla.org/ru/docs/Web/CSS)
+- [JavaScript](https://developer.mozilla.org/ru/docs/Web/JavaScript)
+
+## Author
+
+Cristina Alekseeva
--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -11,20 +11,49 @@ function initGame() {
   usersGuessesLeft.textContent = guessesLeft;
   lettersGuessed = "";
   usersLettersGuessed = document.getElementById("guessed");
-  currentImage = document.getElementById("foto");
+
   band = getRandom(bands);
-  currentImage.setAttribute("src", band.link);
+
   currentWord = writeName(band.name);
   usersLettersGuessed.textContent = lettersGuessed;
 }
 
 var bands = [
-  { link: "assets/images/band1.jpeg", name: "MISFITS" },
-  { link: "assets/images/band2.jpg", name: "RAMONES" },
-  { link: "assets/images/band3.jpg", name: "RAMMSTEIN" },
-  { link: "assets/images/band4.jpg", name: "GARBAGE" },
-  { link: "assets/images/band5.jpg", name: "NIRVANA" },
-  { link: "assets/images/band6.jpg", name: "KORN" }
+  {
+    link: "assets/images/band1.jpeg",
+    name: "MISFITS",
+    musLink:
+      "http://98.157.74.38/MUSIC/Jack%20Connoly/The%20Misfits/Unknown%20album/Misfits%20-%20Mommy,%20Can%20I%20Go%20Out%20And%20Kill%20Tonight.0001.mp3"
+  },
+  {
+    link: "assets/images/band2.jpg",
+    name: "RAMONES",
+    musLink: "http://9092.ultratv100.com:9090/music/The%20Ramones/"
+  },
+  {
+    link: "assets/images/band3.jpg",
+    name: "RAMMSTEIN",
+    musLink:
+      "http://hcmaslov.d-real.sci-nnov.ru/public/mp3/Rammstein/Rammstein%20'Du%20Hast'.mp3"
+  },
+  {
+    link: "assets/images/band4.jpg",
+    name: "GARBAGE",
+    musLink:
+      "http://hcmaslov.d-real.sci-nnov.ru/public/mp3/Garbage/Garbage%20'Cherry%20Lips%20(Go%20Baby%20Go)'.mp3"
+  },
+  {
+    link: "assets/images/band5.jpg",
+    name: "NIRVANA",
+    musLink:
+      "http://hcmaslov.d-real.sci-nnov.ru/public/mp3/Nirvana/Nirvana%20'Come%20As%20You%20Are'.mp3"
+  },
+  {
+    link: "assets/images/band6.jpg",
+    name: "KORN",
+    musLink:
+      "http://98.157.74.38/MUSIC/TYLER/Korn/Greatest%20Hits%20Vol.1/19%20Freak%20On%20A%20Leash%20%5bDante%20Ross%20Mix%5d.mp3"
+  }
 ];
 
 function getRandom(list) {
@@ -68,18 +97,36 @@ function game() {
         .sort()
         .join() === usersWord.sort().join()
     ) {
-      alert("YOU WIN!");
-      location.reload();
-      return;
+      currentImage = document.getElementById("foto");
+
+      currentImage.setAttribute("src", band.link);
+      // debugger;
+      playMusic(band.musLink);
+
+      setTimeout(function() {
+        alert("YOU WIN!");
+        location.reload();
+      }, 100);
     }
 
     guessesLeft--;
     usersGuessesLeft.textContent = guessesLeft;
     if (guessesLeft === 0) {
-      alert("YOU LOST! THE BAND NAME'S IS " + band.name);
-      location.reload();
+      // debugger;
+      currentImage = document.getElementById("foto");
+      currentImage.setAttribute("src", band.link);
+      playMusic(band.musLink);
+
+      setTimeout(function() {
+        alert("YOU LOST! THE BAND NAME'S IS " + band.name);
+        location.reload();
+      }, 100);
     }
   };
 }
 
+function playMusic(url) {
+  document.getElementById("player").src = url;
+  document.getElementById("player").play();
+}
 window.onload = game;
